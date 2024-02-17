948db2bc3650468a41161cd5f28c01b76d9e4ae5
Hikmatullo2904 <hikmatulloanvarbekov001@gmail.com>
2024-02-17 15:22:34 +0500

    cashing example with spring-boot-starter-data-redis done

--- src/main/java/com/hikmatullo/cashing/CashingApplication.java
+++ src/main/java/com/hikmatullo/cashing/CashingApplication.java
@@ -2,8 +2,10 @@ package com.hikmatullo.cashing;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 
 @SpringBootApplication
+@EnableCaching
 public class CashingApplication {
 
     public static void main(String[] args) {
