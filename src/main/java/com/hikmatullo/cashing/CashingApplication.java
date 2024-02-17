b1e781213d0754e71f3ad7aa61880747bb01fee1
Hikmatullo2904 <hikmatulloanvarbekov001@gmail.com>
2024-02-17 15:35:42 +0500

    cashing example with caffeine done

--- src/main/java/com/hikmatullo/cashing/CashingApplication.java
+++ src/main/java/com/hikmatullo/cashing/CashingApplication.java
@@ -2,8 +2,13 @@ package com.hikmatullo.cashing;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.CacheConfig;
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.context.annotation.Import;
 
 @SpringBootApplication
+@EnableCaching
+@Import(CacheConfig.class)
 public class CashingApplication {
 
     public static void main(String[] args) {
---
+++ src/main/java/com/hikmatullo/cashing/config/CacheConfig.java
@@ -0,0 +1,32 @@
+package com.hikmatullo.cashing.config;
+
+import org.springframework.cache.CacheManager;
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.cache.caffeine.CaffeineCacheManager;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import com.github.benmanes.caffeine.cache.Caffeine;
+
+
+import java.util.concurrent.TimeUnit;
+
+@Configuration
+@EnableCaching
+public class CacheConfig {
+
+    @Bean
+    public Caffeine caffeineConfig() {
+        return Caffeine.newBuilder()
+                .initialCapacity(100)
+                .maximumSize(500)
+                .expireAfterAccess(60, TimeUnit.MINUTES);
+    }
+
+    @Bean
+    public CacheManager cacheManager(Caffeine caffeine) {
+        CaffeineCacheManager caffeineCacheManager = new CaffeineCacheManager();
+        caffeineCacheManager.setCaffeine(caffeine);
+        return caffeineCacheManager;
+    }
+
+}
